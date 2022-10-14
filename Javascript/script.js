f73fd3fe61d830ad442d76e716d6ce50212f2558
MuhammadFahmeedKhan <fahmeedkhan77@gmail.com>
2022-10-14 22:43:41 +0500

    Navbar

---
+++ Javascript/script.js
@@ -0,0 +1,7 @@
+let navbar = document.getElementById("navbar");
+let bars = document.getElementById("bars");
+
+function toggleNav(){
+    navbar.classList.toggle("show");
+    bars.classList.toggle("fa-xmark");
+}
